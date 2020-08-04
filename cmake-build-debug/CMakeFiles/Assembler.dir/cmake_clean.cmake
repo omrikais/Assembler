1c7f8d5452008095c5161288dace38ed8e9c4c16
Omri Kaisari <omrikais@me.com>
2020-08-04 10:42:50 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Assembler.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Assembler"
+  "Assembler.pdb"
+  "CMakeFiles/Assembler.dir/main.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Assembler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
